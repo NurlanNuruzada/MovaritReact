b034418f011c68d86964249a55c38e3137c6714f
nurlannuruzada <nurlangn@code.edu.az>
2024-04-12 01:11:04 +0400

    Added template

--- src/Pages/Home/HomePageComponents/Section1/Section1.jsx
+++ src/Pages/Home/HomePageComponents/Section1/Section1.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
-import CustomizeImage from '../../../../Images/Customize.svg'
-import Styles from './Section1.module.css'
-import WarningImage from '../../../../Images/warning.svg'
-import { Grid, GridItem } from '@chakra-ui/react'
+
 export default function Section1() {
   return (
-    <div className={Styles.MainContainer}>
-      <div className={Styles.BetaVersionSection}>
-        <div><img src={WarningImage} alt="" />This is a beta version!</div>
-      </div>
-      <Grid gridTemplateColumns={"1fr 1fr"} gap={32} justifyContent="space-between" className={Styles.SecconSection}>
-        <GridItem className={Styles.LeftContainer}>
-          <h1 className={Styles.MainTitle}>Engage with your website visitors using messaging applications.</h1>
-          <h2 className={Styles.Desc}>Customize the chat widget to match your brand's colors and style. Additionally,
-            set up a welcome message or chatbot to greet visitors and guide them to the right information.</h2>
-          <button className={Styles.GetButton}>Get your free button</button>
-        </GridItem>
-        <GridItem  className={Styles.RightContainer}>
-          <img className={Styles.Image} src={CustomizeImage} alt="" />
-        </GridItem>
-      </Grid>
-
-    </div>
+    <div>Section1</div>
   )
 }
