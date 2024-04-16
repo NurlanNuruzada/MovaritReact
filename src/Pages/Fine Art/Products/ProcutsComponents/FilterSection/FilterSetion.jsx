d33bf3c49bca64b422ec29352274d8c559eaa920
nurlannuruzada <nurlangn@code.edu.az>
2024-04-16 10:23:50 +0400

    MoodArt adedd

--- src/Pages/Fine Art/Products/ProcutsComponents/FilterSection/FilterSetion.jsx
+++ src/Pages/Fine Art/Products/ProcutsComponents/FilterSection/FilterSetion.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import Styles from './FilterSection.module.css'
 import Filter from '../../../../../Components/Filters/Filter'
 import CustomFilterSection from '../../../../../Components/CustomFilterSection/CustomFilterSection'
-export default function FilterSetion() {
-    const Sellections = ["All", "Recomended", "Mostly Viewed", "RecentlyAdded"]
+export default function FilterSetion({Sellections,Default}) {
     return (
         <div className={Styles.Main}>
-            <Filter  SellectOptions={Sellections} />
+            <Filter Default={Default}  SellectOptions={Sellections} />
         </div>
     )
 }
